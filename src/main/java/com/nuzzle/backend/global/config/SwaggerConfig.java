7e08ac087618e010acee4ce9947f6e5628b8e2da
leeeunda <daun0219@gmail.com>
2024-08-15 23:48:45 +0900

    feat: Swagger 설정 수정

--- src/main/java/com/nuzzle/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/nuzzle/backend/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,26 +14,45 @@ import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+
+
+                .build();
+    }
+
     @Bean
     public OpenAPI openAPI() {
+
+        // Security Scheme 명
         String jwt = "JWT";
+
+        // API 요청 헤더에 인증정보 포함
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+
+        // Security Scheme 등록
         Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
                 .name(jwt)
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT")
         );
+
         return new OpenAPI()
                 .components(new Components())
                 .info(apiInfo())
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
+
     private Info apiInfo() {
         return new Info()
-                .title("API Test") // API의 제목
-                .description("Let's practice Swagger UI") // API에 대한 설명
-                .version("1.0.0"); // API의 버전
+                .title("Nuzzle API") // API의 제목
+                .description("Nuzzle API Docs") // API에 대한 설명
+                .version("1.0.0"); // 문서 버전
     }
 }
