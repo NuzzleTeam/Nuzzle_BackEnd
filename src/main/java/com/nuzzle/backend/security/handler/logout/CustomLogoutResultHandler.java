fd04d59bc28ac7141266e55fc9dc8adf2c7b4423
leeeunda <daun0219@gmail.com>
2024-08-17 20:15:13 +0900

    feat: CustomAuthentication

---
+++ src/main/java/com/nuzzle/backend/security/handler/logout/CustomLogoutResultHandler.java
@@ -0,0 +1,58 @@
+package com.nuzzle.backend.security.handler.logout;
+
+import com.nuzzle.backend.global.dto.ExceptionDto;
+import com.nuzzle.backend.global.exception.ErrorCode;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import net.minidev.json.JSONValue;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class CustomLogoutResultHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        // 애초에 로그인이 안된 경우
+        if (authentication == null) {
+            setFailureResponse(response);
+            return;
+        }
+
+        setSuccessResponse(response);
+    }
+
+    private void setSuccessResponse(HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpStatus.OK.value());
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("success", true);
+        result.put("data", null);
+        result.put("error", null);
+
+        response.getWriter().write(JSONValue.toJSONString(result));
+    }
+
+    private void setFailureResponse(HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("success", false);
+        result.put("data", null);
+        result.put("error", ExceptionDto.of(ErrorCode.NOT_FOUND_LOGIN_USER));
+
+        response.getWriter().write(JSONValue.toJSONString(result));
+    }
+}
