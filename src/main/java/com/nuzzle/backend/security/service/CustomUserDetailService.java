6d67a5d0ec6ce618a1a613e01a44833742b1ecc2
leeeunda <daun0219@gmail.com>
2024-08-17 20:15:13 +0900

    feat: CustomAuthentication

---
+++ src/main/java/com/nuzzle/backend/security/service/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.nuzzle.backend.security.service;
+
+
+import com.nuzzle.backend.global.exception.CommonException;
+import com.nuzzle.backend.global.exception.ErrorCode;
+import com.nuzzle.backend.security.info.UserPrincipal;
+import com.nuzzle.backend.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserRepository.UserSecurityForm user = userRepository.findSecurityFormBySerialId(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return UserPrincipal.create(user);
+    }
+
+    public UserDetails loadUserById(Long id) {
+        UserRepository.UserSecurityForm user = userRepository.findSecurityFormById(id)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_LOGIN_USER));
+
+        return UserPrincipal.create(user);
+    }
+}
