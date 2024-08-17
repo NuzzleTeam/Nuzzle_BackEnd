a3eefe32798cea98a43347fffe329d28851ffc7c
leeeunda <daun0219@gmail.com>
2024-08-17 20:16:46 +0900

    feat: User Service

--- src/main/java/com/nuzzle/backend/user/repository/UserRepository.java
+++ src/main/java/com/nuzzle/backend/user/repository/UserRepository.java
@@ -1,7 +1,61 @@
 package com.nuzzle.backend.user.repository;
 
+import com.nuzzle.backend.global.dto.type.ERole;
 import com.nuzzle.backend.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserIdAndRefreshTokenAndIsLogin(Long id, String refreshToken, Boolean isLogin);
+
+    User findByUserName(String UserName);
+
+    @Query("select u.userId as id, u.role as role, u.password as password from User u where u.serialId = :serialId")
+    Optional<UserSecurityForm> findSecurityFormBySerialId(String serialId);
+
+    @Query("select u.userId as id, u.role as role, u.password as password from User u where u.userId = :id and u.isLogin = true")
+    Optional<UserSecurityForm> findSecurityFormById(Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update User u set u.refreshToken = :refreshToken, u.isLogin = :isLogin where u.userId = :userId")
+    void updateRefreshTokenAndLoginStatus(Long userId, String refreshToken, Boolean isLogin);
+
+    boolean existsByUserName(String userName);
+
+    Boolean existsBySerialId(String serialId);
+
+    Optional<User> findBySerialId(String serialId);
+
+    interface UserSecurityForm {
+        Long getId();
+
+        ERole getRole();
+
+        String getPassword();
+
+        static UserSecurityForm invoke(User user) {
+            return new UserSecurityForm() {
+                @Override
+                public Long getId() {
+                    return user.getUserId();
+                }
+
+                @Override
+                public ERole getRole() {
+                    return user.getRole();
+                }
+
+                @Override
+                public String getPassword() {
+                    return user.getPassword();
+                }
+            };
+        }
+    }
+
 }
+
+
--- src/main/java/com/nuzzle/backend/user/service/UserService.java
+++ src/main/java/com/nuzzle/backend/user/service/UserService.java
@@ -1,7 +1,35 @@
 package com.nuzzle.backend.user.service;
 
+import com.nuzzle.backend.global.exception.CommonException;
+import com.nuzzle.backend.global.exception.ErrorCode;
+import com.nuzzle.backend.user.dto.UserResponseDto;
+import com.nuzzle.backend.user.repository.UserRepository;
 import com.nuzzle.backend.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-    User getUserById(Long userId); // 유저 정보를 ID로 가져오는 메서드
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserResponseDto readUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
+
+        return UserResponseDto.fromEntity(user);
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
+    }
+
+    public boolean checkDuplicate(String userName) {
+        return userRepository.existsByUserName(userName);
+    }
+
+
+
 }
